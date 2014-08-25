4bec4c2ef9c5e8adcd8e9fc61306ef95ebed89f7
Michael Bova <bovavob@gmail.com>
2014-08-25 17:20:01 -0500

    final commit for branch?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,6 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -75,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Disable automatic flushing of the log to improve performance.
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development? || Rails.env.production?
     port:           '587',
     user_name:      ENV['MANDRILL_USERNAME'],
     password:       ENV['MANDRILL_APIKEY'],
-    domain:         'rails-wiki.herokuapp.com',
+    domain:         'herokuapp.com',
     enable_starttls_auto: true
   }
 end
